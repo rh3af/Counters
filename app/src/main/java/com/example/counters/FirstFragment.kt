f8edd81cfa2026a6adddecd1f42288179b73c5e7
C Hemanth <ram.hemanth3@gmail.com>
2023-08-11 20:56:09 -0300

    Reverted to previous working state

--- app/src/main/java/com/example/counters/FirstFragment.kt
+++ app/src/main/java/com/example/counters/FirstFragment.kt
@@ -1,4 +1,4 @@
-/*package com.example.counters
+package com.example.counters
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.counters.databinding.FragmentFirstBinding
 
 /**
@@ -33,73 +31,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
-        val titles = listOf("Title 1", "Title 2", "Title 3") //
-        val titleAdapter = TitleAdapter(titles) { clickedTitle ->
-            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(clickedTitle)
-            findNavController().navigate(action)
-        }
-
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = titleAdapter
-        }
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
-
-*/
-
-package com.example.counters
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.counters.databinding.FragmentFirstBinding
-
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
-
-    private var _binding: FragmentFirstBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val recyclerView: RecyclerView = binding.recyclerView
-        val titles = listOf("Title 1", "Title 2", "Title 3") // Add your titles
-        val titleAdapter = TitleAdapter(titles) { clickedTitle ->
-            val directions = FirstFragmentDirections.actionFirstFragmentToSecondFragment(clickedTitle)
-            val navController = NavHostFragment.findNavController(this)
-            navController.navigate(directions)
-        }
-
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = titleAdapter
-        }
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
